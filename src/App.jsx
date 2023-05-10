2b03d0854037961e28fd9ca6c92cb242112fb158
matei <mateidr7@gmail.com>
2023-05-10 15:47:42 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/da" element={<><h1>ok</h1></>} />
         <Route path="/" element={<Assembly />} />
       </Routes>
     </BrowserRouter>
