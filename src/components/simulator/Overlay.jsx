fd4c74dccec37f7ee9575837a423fdd4391fc136
matei <mateidr7@gmail.com>
2023-05-12 11:29:49 +0300

    better styling

--- src/components/simulator/Overlay.jsx
+++ src/components/simulator/Overlay.jsx
@@ -5,14 +5,14 @@ function Overlay() {
   return (
     <div className="wrapper_MAMA_CUIVA">
       <div className="controls">
-        <div>Camere: 1 - 9</div>
-        <div>Camera pe robot: 0</div>
-        <div>CONTROL: W A S D</div>
-        <div>STRAFES: Q și E</div>
-        <div>ROTIREA ST-DR: SĂGEȚI</div>
-        <div>SUS - JOS BRAT: SĂGEȚI</div>
-        <div>ARUNCĂ CON: F (ȚINE APĂSAT)</div>
-        <div>RESETARE: R</div>
+        <h5>CAMERA PE ROBOT: 0</h5>
+        <h5>CAMERE: 1 - 9</h5>
+        <h5>CONTROL: W A S D</h5>
+        <h5>STRAFES: Q și E</h5>
+        <h5>ROTIREA ST-DR: SĂGEȚI</h5>
+        <h5>SUS - JOS BRAT: SĂGEȚI</h5>
+        <h5>ARUNCĂ CON: F (ȚINE APĂSAT)</h5>
+        <h5>RESETARE: R</h5>
         {/* press k to swap camera<br /> */}
         {/* Teren stricat sau robot ce se misca ciudat?<br />
             Apasa R pentru a reseta. */}
